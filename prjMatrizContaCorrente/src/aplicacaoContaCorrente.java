886753878e8d69523b61ed3e3552f6ab0e1c6d11
Antonio Carlos Fernandes Gomes Filho <antoniocarlosfilho2003@gmail.com>
2023-03-28 10:05:30 -0300

    Dia 21 - Matriz e Pesquisa Linear

--- prjMatrizContaCorrente/src/aplicacaoContaCorrente.java
+++ prjMatrizContaCorrente/src/aplicacaoContaCorrente.java
@@ -1,66 +1,128 @@
 import fatec.poo.model.contaBancaria;
 import java.text.DecimalFormat;
 import java.util.Scanner;
+
 public class aplicacaoContaCorrente {
 
     public static void main(String[] args) {
         
-        int numero,opcao;
-        double saldo,valor;
+        int opcao,i;
+        double valor;
+        int num = 15;
         
         Scanner operacao = new Scanner(System.in);
         DecimalFormat df = new DecimalFormat("#,##0.00");
+        contaBancaria[] banco = new contaBancaria[num];
         
-        System.out.print("Digite o numero da conta: ");
-        numero = operacao.nextInt();
+        int numero;
+        double saldo;
         
-        System.out.print("Digite o saldo da conta:R$ ");
-        saldo = operacao.nextDouble();
+        for (i =0; i < num; i++)
+        {
+            System.out.print("Digite o numero da conta: ");
+            numero = operacao.nextInt();
         
-        contaBancaria objConta = new contaBancaria(numero, saldo);
+            System.out.print("Digite o saldo da conta:R$ ");
+            saldo = operacao.nextDouble();
+        
+            banco[i] = new contaBancaria(numero, saldo);
+        }
         
         do {
             
             System.out.println("\n1 –Depositar");
             System.out.println("2 –Sacar");
             System.out.println("3 –Consultar Saldo");
-            System.out.println("4 - Sair");
+            System.out.println("4 - Listar Contas");
+            System.out.println("5 - Sair");
             
             System.out.print("\n\nDigite uma opcao: ");
             opcao = operacao.nextInt();
             
             switch (opcao) {
                 case 1:
-                    System.out.println("Numero da conta: "+objConta.getNumero());
-                    System.out.print("Digite o valor a ser Depositado: R$ ");
-                    valor = operacao.nextDouble();
-                    objConta.depositar(valor);
-                    break;
-                case 2:
-                    System.out.println("Numero da conta: "+objConta.getNumero());
-                    System.out.print("Digite o valor a ser Sacado: R$ ");
-                    valor = operacao.nextDouble();
-                    if(objConta.getSaldo() < valor)
+                    System.out.print("Numero da conta a pesquisar: ");
+                    numero = operacao.nextInt();
+                    for(i=0;i<15; i++)
                     {
-                        System.out.println("Saldo Insuficiente");
-                        break;
+                        if(banco[i].getNumero() == numero)
+                        {
+                            break;
+                        }
                     }
-                    else
+                    if(i == 15)
                     {
-                        objConta.sacar(valor);
+                      System.out.print("Conta não Cadastrada");  
                     }
                     
+                    else
+                    {
+                        System.out.println("Numero da conta: "+banco[i].getNumero());
+                        System.out.print("Digite o valor a ser Depositado: R$ ");
+                        valor = operacao.nextDouble();
+                        banco[i].depositar(valor);   
+                    }
+                    break;
+                case 2:
+                    System.out.print("Numero da conta a pesquisar: ");
+                    numero = operacao.nextInt();
+                    for(i=0;i<15; i++)
+                    {
+                        if(banco[i].getNumero() == numero)
+                        {
+                            break;
+                        }
+                    }
+                    if(i == 15)
+                    {
+                      System.out.print("Conta não Cadastrada");  
+                    }
+                    
+                    else
+                    {
+                        if(banco[i].getNumero() == numero)
+                        {
+                            System.out.println("Numero da conta: "+banco[i].getNumero());
+                            System.out.print("Digite o valor a ser Sacado: R$ ");
+                            valor = operacao.nextDouble();
+                        if(banco[i].getSaldo() < valor)
+                        {
+                            System.out.println("Saldo Insuficiente");
+                        }
+                        else
+                        {
+                            banco[i].sacar(valor);
+                        }
+                        }   
+                    }
                     break;
                 case 3:
-                    System.out.println("Numero da conta: "+objConta.getNumero());
-                    System.out.println("Saldo: R$ "+df.format(objConta.getSaldo()));
-                    break;       
-            }
-            
-            
-        }while(opcao != 4);
-        
-        
+                    System.out.print("Numero da conta a pesquisar: ");
+                    numero = operacao.nextInt();
+                    for (i = 0; i < num; i++)
+                    {
+                        if(banco[i].getNumero() == numero)
+                        {
+                            break;
+                        }
+                    }
+                    if(i == 15)
+                    {
+                       System.out.println("Conta nao cadastrada!"); 
+                    }
+                    else{
+                         System.out.println("Numero da conta: "+banco[i].getNumero()+"\nSaldo: R$"+df.format(banco[i].getSaldo()));
+                         System.out.println(banco[i].getNumero()+ "\t\t\tR$ "+df.format(banco[i].getSaldo()));
+                    }
+                    break;
+                case 4:
+                     System.out.println("Numero da conta: "+"\tSaldo:");
+                     for (i = 0; i < num; i++)
+                    {
+                        System.out.println(banco[i].getNumero()+ "\t\tsR$ "+df.format(banco[i].getSaldo()));
+                    }
+                break;
+            }  
+        }while(opcao != 5);
     }
-    
 }
